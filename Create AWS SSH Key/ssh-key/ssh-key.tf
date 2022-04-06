ef8c5743ce9ae480e43a5e22550bea1a0294c8fb
sonal-Kumar22 <101549112+sonal-Kumar22@users.noreply.github.com>
2022-04-07 02:13:28 +0530

    Code updated

--- Create AWS SSH Key/ssh-key/ssh-key.tf
+++ Create AWS SSH Key/ssh-key/ssh-key.tf
@@ -1,8 +1,18 @@
+resource "aws_instance" "web" {
+  ami           = "ami-08ca3fed11864d6bb"
+  instance_type = "t2.micro"
+  key_name = "${aws_key_pair.key_tf.key_name}"
+  tags = {
+    Name = "EC2-Sonal"
+  }
+}
+
 resource "aws_key_pair" "key_tf" {
-  key_name   = "terraform-key"
+  key_name   = "terraform-key" #this attached the key to EC2 instance
   public_key = file("${path.module}/id_rsa.pub")
 }
 
 output "keyname" {
-  value = aws_key_pair.key_tf.key_name
+  value = "${aws_key_pair.key_tf.key_name}"
 }
+
